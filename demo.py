8812fa537260d427a40a2f0aa223ad4e177db8a1
yasasvi <yasasvigumma@gmail.com>
2023-01-28 07:35:33 +0530

    changes

--- demo.py
+++ demo.py
@@ -1,12 +1,27 @@
 import datetime as dt
 import time
+from sys import platform
+import socket
 
-endDate = dt.datetime()  # Give a date like (2003 : year, 12 : month, 15 : date)
-siteBlock = ["www.youtube.com", "www.facebook.com",
+# please check if there is a host file or not
+
+if platform == "linux" or platform == "linux2":
+    path = '/etc/hosts'
+    port = socket.gethostname()
+elif platform == "darwin":
+    path = '/etc/hosts'
+    port = socket.gethostname()
+elif platform == "win32":
+    path = 'C:/Windows/System32/drivers/etc'
+    port = socket.gethostname()
+
+# Give a date like (2003 : year, 12 : month, 15 : date)
+endDate = dt.datetime(2023, 1, 28)
+siteBlock = ["www.facebook.com",
              "www.instagram.com"]  # you can add you websites
-# your host file (it will be in c:\Windows\System32\drivers\etc folder)
-hostPath = ""
-redirect = ""  # use your localhost number
+
+hostPath = path
+redirect = port
 
 while True:
     if dt.datetime.now() < endDate:
